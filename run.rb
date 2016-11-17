1c91cb1b7d01ce8e7a1bd90006e4c8209b6ee699
Robert Paskowitz <robert.paskowitz@upsight.com>
2016-11-16 21:36:08 -0800

    Allow copying events into the original base calendar.

--- run.rb
+++ run.rb
@@ -65,12 +65,12 @@ def new_calendar(name, time_zone)
   $service.insert_calendar(future_cal)
 end
 
-def copy_events(base_cal, new_cal, offset_days = 0)
+def copy_events(base_cal, destination_cal, offset_days = 0)
   return puts 'No events found' if base_cal.items.empty?
   base_cal.items.each do |event|
     new_start_time = event.start.date_time + offset_days
     new_end_time = event.end.date_time + offset_days
-    new_event(new_cal.id, event.summary, event.description, new_start_time.iso8601, new_end_time.iso8601, 'Asia/Hong_Kong')
+    new_event(destination_cal, event.summary, event.description, new_start_time.iso8601, new_end_time.iso8601, 'Asia/Hong_Kong')
   end
 end
 
@@ -79,14 +79,21 @@ $service = Google::Apis::CalendarV3::CalendarService.new
 $service.client_options.application_name = APPLICATION_NAME
 $service.authorization = authorize
 
-puts 'URL of base calendar'
-base_cal = $service.list_events(gets.chomp, single_events: true, order_by: 'startTime', time_min: '1970-01-01T00:00:00Z')
+puts 'ID of base calendar'
+base_cal_id = gets.chomp
+base_cal = $service.list_events(base_cal_id, single_events: true, order_by: 'startTime', time_min: '1970-01-01T00:00:00Z')
 
-puts 'Name of your new calendar'
-new_cal = new_calendar(gets, base_cal.time_zone)
+puts 'Name of your new calendar(blank to copy to source)'
+new_cal_name = gets.chomp
+if new_cal_name.length > 0
+  new_cal = new_calendar(new_cal_name, base_cal.time_zone)
+  destination_cal = new_cal.id
+else
+  destination_cal = base_cal_id
+end
 
 puts 'Set your offsets in days (default: 0)'
 days_of_offsets = gets
-copy_events(base_cal, new_cal, days_of_offsets.to_i)
+copy_events(base_cal, destination_cal, days_of_offsets.to_i)
 
 puts 'DONE!'
